1053231a188e0f4b6906837f95734daa31e895a5
lakhani12 <hitlakhani3@gmail.com>
2026-03-16 17:46:27 +0530

    yhyh

--- keep-not/src/App.jsx
+++ keep-not/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return <>
+  <section className="lg:flex item-center justify-center">
+    {/* Form for Add new Task */}
+    <div className=" lg:w-1/2 bg-teal-950 w-full h-screen">
+    </div>
+    {/* Div for show all tasks */}
+    <div className=" lg:w-1/2 bg-black w-full h-screen">
+    </div>
+  </section>
+  </>;
 };
 
 export default App;
