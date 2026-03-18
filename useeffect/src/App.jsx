b9e9ecdc0aa9a981658cd5aed428070236306355
lakhani12 <hitlakhani3@gmail.com>
2026-03-18 19:13:31 +0530

    use effect

--- useeffect/src/App.jsx
+++ useeffect/src/App.jsx
@@ -1,10 +1,12 @@
 import ApiFetch from "./Components/ApiFetch"
+import UseEffect from "./Components/UseEffect"
 
 
 const App = () => {
   return (
     <>
-      <ApiFetch/>
+      {/* <ApiFetch/> */}
+      <UseEffect/>
     </>
   )
 }
