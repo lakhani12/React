5218c01302805923a99d7bb668d85baf662c0600
lakhani12 <hitlakhani3@gmail.com>
2026-03-20 19:46:10 +0530

    monday

--- unspalash/src/Components/Gallery.jsx
+++ unspalash/src/Components/Gallery.jsx
@@ -1,53 +1,73 @@
-import React, { useEffect, useState } from "react";
-import { Bookmark, Plus, ArrowDown } from "lucide-react";
+import { ArrowDown, Bookmark, Plus } from "lucide-react";
 import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Gallery = () => {
-  // api
   const [img, setImg] = useState([]);
+  const [page, setPage] = useState(1);
+
+  //api fetch
   useEffect(() => {
     try {
       const FetchImg = async () => {
         let response = await axios.get(
-          "https://picsum.photos/v2/list?page=1&limit=100",
+          `https://picsum.photos/v2/list?page=${page}&limit=100`,
         );
-        console.log(response);
         setImg(response.data);
       };
       FetchImg();
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [page]);
+
+  // scroll effect
+//   useEffect(() => {
+//     const handelScroll = () => {
+//       if (
+//         window.innerHeight + window.scrollY >=
+//         document.documentElement.scrollHeight - 300
+//       ) {
+//         setPage((prev) => prev + 1);
+//       }
+//     };
+//   });
+// Monday
+
   return (
     <>
       <section>
-        {/* Fetch image from Api  */}
         <div className="columns-1 sm:columns-2 md:columns-3 gap-4 p-4">
+          {/* Fetch Images from API */}
           {img.map((data) => {
             return (
-              <div className="relative mb-4 break-inside-avoid shadow group" key={data.id}>
-                {/* image  */}
+              <div
+                key={data.id}
+                className="relative mb-4 break-inside-avoid shadow group"
+              >
+                {/* Image */}
                 <img
                   src={data.download_url}
                   alt="image"
-                  className="object-cover w-full "
+                  className="w-full object-cover"
                 />
 
-                {/* overlay div  */}
-                <div className="absolute top-0 inset-0 bg-black/50 flex flex-col justify-between  p-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
-                  {/* top icon  */}
-                  <div className="flex justify-end gap-2 ">
-                    <div className="bg-white p-2 rounded-md ">
+                {/* Overlay Div */}
+                <div className="absolute top-0 bg-black/50 inset-0 flex flex-col justify-between p-2 opacity-0 group-hover:opacity-100 transition duration-300">
+                  {/* top icons */}
+                  <div className="flex justify-end gap-2">
+                    <div className="bg-white p-2 rounded-md">
                       <Bookmark />
                     </div>
-                    <div className="bg-white p-2 rounded-md ">
+                    <div className="bg-white p-2 rounded-md">
                       <Plus />
                     </div>
                   </div>
-                  {/* bottom icon  */}
-                  <div className="flex items-center justify-between ">
+
+                  {/* bottom icons */}
+                  <div className="flex items-center justify-between">
                     <h1 className="text-white font-bold">{data.author}</h1>
-                    <div className="bg-white p-2 rounded-md ">
+                    <div className="bg-white p-2 rounded-md">
                       <ArrowDown />
                     </div>
                   </div>
