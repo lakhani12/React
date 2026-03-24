7409041119b87e7b5159657fdab8765c9f92d95c
lakhani12 <hitlakhani3@gmail.com>
2026-03-24 19:26:30 +0530

    ecommerce

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import Product from './Pages/Product'
+
+
+const App = () => {
+  return (
+    <>
+    <Routes>
+      <Route>
+        <Route path='/' element={<Home />} />
+           <Route path='/product/:id' element={< Product/>} />
+      </Route>
+    </Routes>
+     
+    </>
+  )
+}
+
+export default App
---
+++ ecommerce/src/Pages/Collection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Collection = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Collection
